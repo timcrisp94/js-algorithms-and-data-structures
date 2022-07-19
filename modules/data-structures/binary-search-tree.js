9c1e891c0308062da386253119842b5724f3bc1a
Tim Crisp <timothycrisp@gmail.com>
2022-07-19 13:12:55 -0500

    add find method to bst

--- modules/data-structures/binary-search-tree.js
+++ modules/data-structures/binary-search-tree.js
@@ -34,6 +34,21 @@ class BinarySearchTree {
           }
       }
   }
+  find(value) {
+    if (this.root === null) return false
+    let current = this.root
+    let found = false
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left
+      } else if (value > current.value) {
+        current = current.right
+      } else {
+        return true
+      }
+    }
+    return false
+  }
 }
 
 const tree = new BinarySearchTree()
